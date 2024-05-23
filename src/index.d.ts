ab086028997f1b907d66deaf842d7d15af9d8284
fernando.basello <>
2024-05-23 01:10:07 -0300

    Some icon test

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
 declare module 'react-native-rough';
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import Rough from 'react-native-rough';
 import RoughView from '../components/RoughView';
 import strings from '../utils/strings';
+import AddMore from '../../assets/imgs/add-sign-bold.svg';
+import { Svg } from 'react-native-svg';
 
 const styles = StyleSheet.create({
   flex: {
@@ -19,6 +22,9 @@ const styles = StyleSheet.create({
   },
 });
 
+const width = 100;
+const padding = 20;
+
 const Home = () => {
   return (
     <View style={styles.flex}>
@@ -31,6 +37,22 @@ const Home = () => {
         <Text style={styles.font} numberOfLines={4}>
           {strings.home_last_action}
         </Text>
+        <AddMore width={50} height={50} strokeWidth={50} />
+        <Svg pointerEvents="none" width={width} height={width}>
+          <Rough.Circle
+            x={50}
+            y={width / 2}
+            diameter={width - padding}
+            hachureAngle={60}
+            hachureGap={5}
+            fillWeight={3}
+            stroke="black"
+            strokeWidth={5}
+            fill="gray"
+          />
+          <Rough.Line x1={padding} y1={width / 2} x2={width - padding} y2={width / 2} stroke="black" strokeWidth={5} />
+          <Rough.Line x1={width / 2} y1={padding} x2={width / 2} y2={width - padding} stroke="black" strokeWidth={5} />
+        </Svg>
       </RoughView>
     </View>
   );
