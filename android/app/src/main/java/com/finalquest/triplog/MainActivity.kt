b0638cb89a5e2a910ac3b7e6bc2c45d6a0d5718d
fernando.basello <>
2024-05-27 23:48:44 -0300

    Added firebase core and analytics

--- android/app/src/main/java/com/finalquest/triplog/MainActivity.kt
+++ android/app/src/main/java/com/finalquest/triplog/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.triplog
+package com.finalquest.triplog
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/finalquest/triplog/MainApplication.kt
+++ android/app/src/main/java/com/finalquest/triplog/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.triplog
+package com.finalquest.triplog
 
 import android.app.Application
 import com.facebook.react.PackageList
