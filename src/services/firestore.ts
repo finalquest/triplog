4eb1b094898a40bb97ac2d57c9b826984fac0aae
fernando.basello <>
2024-05-30 23:53:06 -0300

    Upload file to cloudStorage

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,10 +1,15 @@
 import { firebase } from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 import { PHOTOS_COLLECTION } from '../model/constants';
 import { FirestorePhoto, Result } from '../model/interfaces';
 import { secretFlagVisibility } from '../model/dbSecrets';
 
 export const saveNewImage = async (path: string, location: { long: number; lat: number }): Promise<Result> => {
   try {
+    const uploadImage = await uploadImageToStorage(path);
+    if (!uploadImage.ok) {
+      return uploadImage;
+    }
     const db = firebase.firestore();
     const imageToAdd: FirestorePhoto = {
       url: path,
@@ -20,4 +25,17 @@ export const saveNewImage = async (path: string, location: { long: number; lat: 
     return { error: 'Error Saving firestore document', ok: false };
   }
   return { error: null, ok: true };
+};
+
+const uploadImageToStorage = async (path: string): Promise<Result> => {
+  try {
+    const fileName = path.split('/').pop();
+    const reference = storage().ref(fileName);
+    await reference.putFile(path);
+    console.log('File successfully uploaded!');
+    return { error: null, ok: true };
+  } catch (error) {
+    console.error('Error uploading file: ', error);
+    return { error: 'Error uploading file', ok: false };
+  }
 };
